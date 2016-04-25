3fef0a0a441de5ae4fe4550b05f4a5938c9fe567
Vincent van Hees <vincentvanhees@gmail.com>
2016-04-25 13:46:49 +0200

    more features

--- classification.R
+++ classification.R
@@ -3,43 +3,33 @@ graphics.off()
 load(file="features.RData")
 
 #==========================================
-# Consider starting with PCA to reduce size of data
-#...
-# PCA = prcomp(x=t(DAT),center=TRUE,scale=TRUE,na.action=na.omit, retx=TRUE,tol=0.01)
-# tmp = summary(PCA)
-# DAT = as.data.frame(PCA$rotation)
+# Consider starting with PCA to reduce size of data:
+PCA = prcomp(x=t(DAT),center=TRUE,scale=TRUE,na.action=na.omit, retx=TRUE,tol=0.05)
+tmp = summary(PCA)
+PCArot = as.data.frame(PCA$rotation)
+nPC = which(summary(PCA)$importance[3,] > 0.9)[1]
+DAT = PCArot[,1:nPC]
 #========================================
 # Classification, randomForest
 noepi = which(LAB$definitive_diagnosis == 1)
 epi = which(LAB$definitive_diagnosis == 2)
 all = c(noepi,epi)
+set.seed(300)
+traini = c(sample(x=noepi,size=round(length(noepi)*0.5)),
+           sample(x=epi,size=round(length(epi)*0.5)))
 
-traini = c(sample(x=noepi,size=round(length(noepi)*0.4)),
-           sample(x=epi,size=round(length(epi)*0.4)))
-testi = all[which(all %in% traini == FALSE)]
+# colnames(DATsd) = paste0(colnames(DATsd),"_sd")
+# colnames(DATmed) = paste0(colnames(DATmed),"_med")
+# DAT = cbind(DATsd[traini,],DATmed[traini,])
 
 DAT = DAT[traini,]
 LAB = LAB[traini,]
-# 
-# m  = randomForest(DAT[traini,],LAB$definitive_diagnosis[traini],ntree=500)
-# p = predict(m,DAT[testi,],type="response")
-# x11();plot(p,LAB$definitive_diagnosis[testi],type="p")
-# # # confusion matrix
-# print(table(round(p),LAB$definitive_diagnosis[testi]))
 #========================================
 # Classification, caret
 print("classification")
 library(caret)
-ctrl = trainControl(method = "repeatedcv",number=10,repeats=10)
-grid_rf = expand.grid(.mtry = c(4,8,16,32))
-# DAT = cbind(DAT,)
-
-if (length(which(LAB$definitive_diagnosis == 3)) > 0) { #ignore class 3
-  DAT = DAT[-which(LAB$definitive_diagnosis == 3),]
-  LAB = LAB[-which(LAB$definitive_diagnosis == 3),]
-}
+ctrl = trainControl(method = "repeatedcv",number=5,repeats=5)
+grid_rf = expand.grid(.mtry = c(4,8,16,32,64))
 DAT$diagn = as.factor(LAB$definitive_diagnosis)
-set.seed(300)
 m_rf = train(diagn ~ .,data=DAT,method="rf",metric="Kappa",trControl=ctrl,tuneGrid=grid_rf)
-
 print(m_rf)
--- dataprep.R
+++ dataprep.R
@@ -4,13 +4,47 @@ setwd("/home/vincent/utrecht/EEG-epilepsy-diagnosis")
 datadir = "data/eeg"
 labels = read.csv("data/labels.csv")
 require(reshape); require(entropy)
-require(randomForest); require(signal); require(pracma)
+require(randomForest); require(signal); require(moments)
+# ; require(pracma); require(fractal)
 
-# en.entropy = function(x) { # entropy
-#   b = discretize(x,numBins=100)
-#   en.entropy = entropy::entropy(b)
-# }
-#========================================
+cn = c('Fp2', 'Fp1', 'F8', 'F4', 'Fz', 'F3', 'F7', 
+       'A2','A1','T8','T7','C4', 'Cz', 'C3',  
+       'P8', 'P4', 'Pz', 'P3', 'P7','O2', 'O1') # channel names
+fn = c("mean","sd","entropy","en.entropy","max","min","skewness","median","domfreq","maxpow") # feature names ,"lyapunov"
+
+#--------------------------------------------------------
+# define generic function for feature extraction
+# Note: approx_entropy from pracma package is very slow
+getfeatures = function(x,fn) {
+  # x: dataframe with multiple signals in columns over which the feature is calculated
+  # fn: feature names that need to be calculated
+  en.entropy = function(x) { # entropy
+    b = discretize(x,numBins=100)
+    en.entropy = entropy::entropy(b)
+  }
+  domfreq = function(x) {
+    pp = spectrum(x,plot=FALSE)
+    domfreq = pp$freq[which.max(pp$spec)]
+  }
+  maxpow = function(x) {
+    pp = spectrum(x,plot=FALSE)
+    maxpow = max(pp$spec^2)
+  }
+  t = c()
+  if (length(which(fn=="mean")) >0) t = rbind(t,sapply(x,mean))
+  if (length(which(fn=="sd")) >0) t = rbind(t,sapply(x,sd))
+  if (length(which(fn=="entropy")) >0) t = rbind(t,sapply(x,entropy))
+  if (length(which(fn=="en.entropy")) >0) t = rbind(t,sapply(x,en.entropy))
+  if (length(which(fn=="max")) >0) t = rbind(t,sapply(x,max))
+  if (length(which(fn=="min")) >0) t = rbind(t,sapply(x,min))
+  if (length(which(fn=="skewness")) >0) t = rbind(t,sapply(x,moments::skewness))
+  if (length(which(fn=="median")) >0) t = rbind(t,sapply(x,median))
+  # if (length(which(fn=="lyapunov")) >0) t = rbind(t,sapply(x,lyapunov))
+  if (length(which(fn=="domfreq")) >0) t = rbind(t,sapply(x,domfreq))
+  if (length(which(fn=="maxpow")) >0) t = rbind(t,sapply(x,maxpow))
+  A = as.data.frame(t)
+}
+#--------------------------------------------------------
 # extract patient identifiers from filename
 fnames = list.files(datadir,include.dirs=FALSE,full.names = FALSE)
 myfun = function(x) {
@@ -18,56 +52,91 @@ myfun = function(x) {
 }
 idnames = sapply(fnames,myfun)
 uid = unique(idnames)
-#========================================
+#--------------------------------------------------------
 # Load data and extract features
 files = list.files(datadir,include.dirs=TRUE,full.names = TRUE)
-S = c()
+rowsd = rowmed = S = c()
+cnt = 0
 for (i in 1:length(uid)) { #unique id numbers
+  if (cnt == 10) {
+    prog = round((i/length(uid)) * 1000)/ 10
+    print(paste0(prog," %"))
+    cnt = 0
+  }
+  cnt = cnt + 1
   ind = which(idnames == i) #which epochs belong to this person
   if (length(ind) > 0) {
     data = read.table(files[ind[1]],sep="") # only look at first 4 second epoch for now
     if (nrow(data) >= 4096) { # only use files with at least 4096 data points
       data = data[1:4096,]
       sc = t(data)
+      #--------------------------------------------
       # extract wavelets:
       require(wavelets)
       wtdata = NULL
       for (i in 1:nrow(sc)) {
         a <- as.matrix(sc[i,]) #t(sc[i,])
-        wt <- dwt(a, filter="haar", boundary="periodic")
+        wt <- dwt(a, filter="d4", boundary="periodic")
         wtdata <- rbind(wtdata, unlist(c(wt@W,wt@V[[wt@level]])))
       }
       wtdata <- as.data.frame(wtdata)# all the 12 wavelets that are possible in a 4607 signal
+      
       bands = c(rep(1,2048),rep(2,1024),rep(3,512),rep(4,256),rep(5,128),rep(6,64),rep(7,32),
                 rep(8,16),rep(9,8),rep(10,4),rep(11,2),rep(12,1),rep(0,1)) # see also names(wtdata)
       # extract features from wavelets:
-      for (j in 1:11) {
+      for (j in 1:9) {
         df = as.data.frame(t(wtdata[,which(bands==j)]))
-        A = as.data.frame(rbind(sapply(df,mean),sapply(df,sd),sapply(df,entropy))) #,sapply(df,en.entropy),sapply(df,approx_entropy)
-        colnames(A) <- c( 'Fp2', 'Fp1', 'F8', 'F4', 'Fz', 'F3', 'F7', 
-                          'A2','A1','T8','T7','C4', 'Cz', 'C3',  
-                          'P8', 'P4', 'Pz', 'P3', 'P7','O2', 'O1')
-        A$method = c(paste0("mean",j),paste0("sd",j),paste0("entropy",j)) #"median","entropy",
+        A = getfeatures(df,fn)
+        colnames(A) = cn
+        # also calculate median feature values
+        tmp = c(as.numeric(apply(A,1,median)),idnames[ind[1]],j)
+        tmp = as.data.frame(t(tmp))
+        # also calculate sd feature values
+        tmp2 = c(as.numeric(apply(A,1,sd)),idnames[ind[1]],j)
+        tmp2 = as.data.frame(t(tmp2))
+        # add labels and merge
+        colnames(tmp) = colnames(tmp2) = c(fn,"id","band")
+        rowmed = rbind(rowmed,tmp)
+        rowsd = rbind(rowsd,tmp2)
+        A$method = paste0(fn,j)
         A$id = idnames[ind[1]]
         S = rbind(S,A)
       }
+      #--------------------------------------------
       # extract features from the raw signals:
-      A = as.data.frame(rbind(sapply(data,mean),sapply(data,sd),sapply(data,entropy))) 
-      colnames(A) <- c( 'Fp2', 'Fp1', 'F8', 'F4', 'Fz', 'F3', 'F7', 
-                        'A2','A1','T8','T7','C4', 'Cz', 'C3',  
-                        'P8', 'P4', 'Pz', 'P3', 'P7','O2', 'O1')
-      A$method = c(paste0("mean raw"),paste0("sd raw"),paste0("entropy raw")) #"median","entropy",
+      A = getfeatures(data,fn)
+      colnames(A) <- cn
+      
+      # also calculate median feature values
+      tmp = c(as.numeric(apply(A,1,median)),idnames[ind[1]],0)
+      tmp = as.data.frame(t(tmp))
+      # also calculate median feature values
+      tmp2 = c(as.numeric(apply(A,1,sd)),idnames[ind[1]],0)
+      tmp2 = as.data.frame(t(tmp2))
+      # add labels and merge
+      colnames(tmp) = colnames(tmp2) = c(fn,"id","band")
+      rowmed = rbind(rowmed,tmp)
+      rowsd = rbind(rowsd,tmp2)
+      A$method = paste0(fn," raw")
       A$id = idnames[ind[1]]
       S = rbind(S,A)
       rm(A)
     }
   }
 }
+# summary of features across channels
+rowsd2 = reshape(data=rowsd,timevar="band",idvar="id",direction="wide")
+rowsd3 = rowsd2[which(rowsd2$id %in% labels$DB_number == TRUE),]
+DATsd = rowsd3[,-which(colnames(rowsd3)=="id")]
+rowmed2 = reshape(data=rowmed,timevar="band",idvar="id",direction="wide")
+rowmed3 = rowmed2[which(rowmed2$id %in% labels$DB_number == TRUE),]
+DATmed = rowmed3[,-which(colnames(rowmed3)=="id")]
+# features per channels
 W = reshape(data=S,timevar="method",idvar="id",direction="wide")
 W2 = W[which(W$id %in% labels$DB_number == TRUE),]
 DAT = W2[,-which(colnames(W2)=="id")]
 LAB = labels[which(labels$DB_number %in% W2$id == TRUE),]
-save(DAT,LAB,labels,file="features.RData")
+save(DAT,DATmed,DATsd,LAB,labels,file="features.RData")
 
 # approx.entropy = function(ts) { # aproximate entropy
 #   approx.entropy = pracma::approx_entropy(ts, edim = 2, r = 0.2*sd(ts), elag = 1)
